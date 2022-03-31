f0fcdc416336d4070582367759b72c1340b61411
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2022-04-01 06:14:40 +0900

    :construction: Create tohoku2022r

---
+++ rmarkdown/tohoku2022r/setup.R
@@ -0,0 +1,17 @@
+# library(wtl)
+set.seed(24601)
+knitr::opts_chunk$set(comment = NA)
+# knitr::opts_chunk$set(dev = "ragg_png")
+options(
+  mc.cores = parallel::detectCores(),
+  wtl.printdf.summarize = FALSE,
+  pillar.print_max = 8L,
+  tibble.print_max = 8L,
+  width = 9999L,
+  dplyr.summarise.inform = FALSE,
+  readr.num_columns = 0L,
+  ggplot2.continuous.colour = "viridis",
+  ggplot2.continuous.fill = "viridis"
+)
+registerS3method("print", "tbl", wtl::printdf)
+registerS3method("print", "tbl_df", wtl::printdf)
