ae972ed574585fe5585af7822b141bc917153b9b
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2022-06-03 20:59:58 +0900

    :construction: Update dates, links, code

--- content/tokiomarine2022/_index.md
+++ content/tokiomarine2022/_index.md
@@ -1,6 +1,6 @@
 +++
 title = "統計モデリング概論 DSHC 2021"
-date = 2021-06-30T09:30:00+09:00
+date = 2022-08-17T09:30:00+09:00
 draft = false
 unlisted = false
 +++
@@ -8,7 +8,7 @@ unlisted = false
 [東京海上 Data Science Hill Climb](https://tokiomarine-dshc.com/)
 
 - 講師: 岩嵜航 (東北大学生命科学研究科)
-- 日程: 2021年6月30 09:30--17:30
+- 日程: 2022-08-17,24 09:30--17:30
 - 場所: zoom.us
 
 ## 講義資料
--- rmarkdown/tokiomarine2022/setup.R
+++ rmarkdown/tokiomarine2022/setup.R
@@ -3,15 +3,23 @@ set.seed(24601)
 knitr::opts_chunk$set(comment = NA)
 # knitr::opts_chunk$set(dev = "ragg_png")
 options(
+  devtools.install.args = c("--no-multiarch", "--no-test-load"),
+  repos = c(CRAN = "https://cloud.r-project.org/"),
+  menu.graphics = FALSE,
   mc.cores = parallel::detectCores(),
   wtl.printdf.summarize = FALSE,
+  pillar.print_max = 8L,
   tibble.print_max = 8L,
   width = 9999L,
+  cli.width = 60L,
   dplyr.summarise.inform = FALSE,
   readr.num_columns = 0L,
+  readr.show_progress = FALSE,
+  readr.show_col_types = FALSE,
   ggplot2.continuous.colour = "viridis",
   ggplot2.continuous.fill = "viridis",
   ggplot2.discrete.colour = as.vector(grDevices::palette.colors(palette = "Okabe-Ito")[-1]),
   ggplot2.discrete.fill = as.vector(grDevices::palette.colors(palette = "Okabe-Ito")[-1])
 )
+registerS3method("print", "tbl", wtl::printdf)
 registerS3method("print", "tbl_df", wtl::printdf)
