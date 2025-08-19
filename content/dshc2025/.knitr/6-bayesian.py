0f6c0ab3a89884326b106764183a2ba3b181583c
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2025-08-19 13:36:27 +0900

    :construction: Add practice for conditional probability

--- content/dshc2025/.knitr/6-bayesian.py
+++ content/dshc2025/.knitr/6-bayesian.py
@@ -12,13 +12,42 @@
 # Google Colab の場合はインストールから:
 # ```py
 # !uv pip install 'matplotlib>=3.10' 'seaborn>=0.13' 'statsmodels>=0.15'
-# !uv pip install 'arviz>=0.22' 'cmdstanpy>=1.2.5'
-# import cmdstanpy
-# cmdstanpy.install_cmdstan()
 # ```
 
 # %%
 # %matplotlib inline
+
+# 有病率 Pr(I): この地域の感染者の割合; 事前確率
+prevalence = 0.003
+
+# 感度 Pr(P|I): 感染してる人に陽性判定が出る
+sensitivity = 0.9
+
+# 特異度 Pr(¬P|¬I): 感染してない人に陰性判定が出る
+specificity = 0.99
+
+# 周辺確率 Pr(P)
+p_positive = prevalence * sensitivity + (1 - prevalence) * (1 - specificity)
+
+# 陽性適中率 Pr(I|P) (検査陽性の人が実際に感染者である確率)
+positive_predictive_value = prevalence * sensitivity / p_positive
+print(positive_predictive_value)
+
+# %% [markdown]
+#
+# 🔰 同様に陰性的中率 Pr(¬I|¬P) を計算してみよう
+# %%
+
+# p_negative =
+# negative_predictive_value =
+
+# %% [markdown]
+#
+# 🔰 計算結果が検査性能だけでなく有病率にも依存することを確認しよう
+
+# %%
+
+# %%
 
 # pyright: reportGeneralTypeIssues=false
 # pyright: reportMissingTypeStubs=false
